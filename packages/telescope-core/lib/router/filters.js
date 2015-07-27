95e47cf2f6ac206c2c6efe42375a1eada829a145
Sacha Greif <sacha357@gmail.com>
2015-07-27 15:19:27 +0900

    many Telescope improvements

--- packages/telescope-core/lib/router/filters.js
+++ packages/telescope-core/lib/router/filters.js
@@ -135,7 +135,7 @@ Router._filters = {
 
   setSEOProperties: function () {
 
-    var props = {meta: {}, og: {}};
+    var props = {meta: {}, og: {}, twitter: {}};
     var title = this.getTitle && this.getTitle();
     var description = this.getDescription && this.getDescription();
     var image = Settings.get("siteImage");
@@ -143,12 +143,18 @@ Router._filters = {
     if (!!title) {
       props.title = title;
     }
+    
     if (!!description) {
       props.meta.description = description;
       props.og.description = description;
     }
+    
     if (!!image) {
       props.og.image = image;
+    }
+
+    if (!!Settings.get("twitterAccount")) {
+      props.twitter.site = Settings.get("twitterAccount");
     }
 
     SEO.set(props);
@@ -213,7 +219,7 @@ Meteor.startup( function (){
     // Router.onAfterAction(filters.resetScroll, {except:['posts_top', 'posts_new', 'posts_best', 'posts_pending', 'posts_category', 'all-users']});
     Router.onAfterAction(Events.analyticsInit); // will only run once thanks to _.once()
     Router.onAfterAction(Events.analyticsRequest); // log this request with mixpanel, etc
-    Router.onAfterAction(filters.setSEOProperties);
+    Router.onAfterAction(filters.setSEOProperties, {except: ["post_page", "post_page_with_slug"]}); // post pages have their own SEO logic
     Router.onAfterAction(filters.setCanonical, {only: ["post_page", "post_page_with_slug"]});
 
     // Unload Hooks
