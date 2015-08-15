c38a116561878c08ed9545b9947721015e9a5926
Sacha Greif <sacha357@gmail.com>
2015-08-15 12:04:38 +0900

    theme update; make prerender optional

--- packages/screenings/lib/server/prerender.js
+++ packages/screenings/lib/server/prerender.js
@@ -1,8 +1,10 @@
-var prerender = Npm.require('prerender-node')
-  .set('protocol', 'http')
-  .set('host', 'screenings.io')
-  .set('prerenderToken', Meteor.settings.PrerenderIO.token);
+if (Meteor.settings.PrerenderIO) {
+  var prerender = Npm.require('prerender-node')
+    .set('protocol', 'http')
+    .set('host', 'screenings.io')
+    .set('prerenderToken', Meteor.settings.PrerenderIO.token);
 
-Meteor.startup(function() {
-  WebApp.rawConnectHandlers.use(prerender);
-});
+  Meteor.startup(function() {
+    WebApp.rawConnectHandlers.use(prerender);
+  });
+}
